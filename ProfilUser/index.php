ea387c6fc44e61b525826a527b4cbcf287ce515c
talel <talelmejri8@gmail.com>
2022-11-19 17:46:41 +0100

    fix bugs

--- ProfilUser/index.php
+++ ProfilUser/index.php
@@ -1,11 +1,12 @@
 <?php
     session_start();
-
+    include "../classes/classes.php";
     if(!isset($_SESSION['nameUser'])){
         header("location:../auth/login");
         exit;
     }
-
+    $user=new user_manager();
+    $mode_visibiltes=$user->getModeAffichage($_SESSION['idUser']);
     $show=false;
     $template="ProfilUser";
     $page_titel=$_SESSION['nameUser'];
