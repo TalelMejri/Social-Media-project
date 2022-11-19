03623d63d1efb4f7d3e031208c26f2ad8e69635d
talel <talelmejri8@gmail.com>
2022-11-19 17:50:50 +0100

    add mode isibilites in lis amis

--- listamis/index.php
+++ listamis/index.php
@@ -5,7 +5,7 @@ include "../classes/classes.php";
 
 $user=new user_manager();
 $all_friends=$user->get_all_friend($_SESSION['idUser']);
-
+$mode_visibiltes=$user->getModeAffichage($_SESSION['idUser']);
 $show=false;
 $template="listamis";
 $page_titel="Amis";
