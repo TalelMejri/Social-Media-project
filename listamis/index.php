7df5b1f9ce35013e11ee620a28210dcac9dbee99
talel <talelmejri8@gmail.com>
2022-11-20 22:15:28 +0100

    condition si amis

--- listamis/index.php
+++ listamis/index.php
@@ -5,7 +5,18 @@ include "../classes/classes.php";
 
 $user=new user_manager();
 $all_friends=$user->get_all_friend($_SESSION['idUser']);
+$all_user_filter=[];
+foreach($all_friends as $item){
+    if($user->get_user_filter_friend($item['iduser'],$_SESSION['idUser'])){
+        array_push($all_user_filter,$item);
+    }
+}
+if(empty($all_user_filter)){
+    $all_user_filter="vide";
+}
+
 $mode_visibiltes=$user->getModeAffichage($_SESSION['idUser']);
+
 $show=false;
 $template="listamis";
 $page_titel="Amis";
