045d65703df8abcca5812f17b841434e738c6a6c
talel <talelmejri8@gmail.com>
2022-11-23 11:19:47 +0100

    list invi user avec get changer

--- get_all_user/index.php
+++ get_all_user/index.php
@@ -1,5 +1,9 @@
 <?php 
 
+   if(isset($_GET['changer'])){
+        include "../classes/classes.php";
+        session_start(); 
+   }
 
     $user=new user_manager();
     $all_user=$user->get_all_users($_SESSION['idUser']);
@@ -14,7 +18,15 @@
         $user->accepter_ami($_GET['id_confirmer'],$_SESSION['idUser']);
         $user->add_notification($_GET['id_confirmer'],$_SESSION['nameUser'].'a accepté votre invitation');
     }
+    if(isset($_GET['changer'])){
+        $show=false;
+        $template="get_all_user";
+        $mode_visibiltes=$user->getModeAffichage($_SESSION['idUser']);
+        $page_titel=$_SESSION['nameUser'];
+        include "../layout.phtml";
+    }else{
+        include "get_all_user.phtml";
+    }
 
-    include "get_all_user.phtml";
 
 ?>
