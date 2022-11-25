03db1770230f5e37a2d3e4aa72689e875e0cedd0
Talel Mejri <96019025+TalelMejri@users.noreply.github.com>
2022-11-25 22:31:18 +0100

    fix bugs

--- delete_notif.php
+++ delete_notif.php
@@ -1,13 +1,5 @@
-<?php
-    session_start();
-    if(!isset($_SESSION['idUser'])){
-        header("location:../auth/login");
-        exit;
-    }
-    if(!array_key_exists('id',$_GET)){
-        header("location:../auth/login");
-        exit;
-    }
+<?php 
+
     include "./classes/classes.php";
     $user=new user_manager();
     $user->delete_notif_by_id($_GET['id']);
