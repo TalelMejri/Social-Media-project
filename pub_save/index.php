743a65e184bc317ae71cb790e1f0858f71828818
talel <talelmejri8@gmail.com>
2022-11-26 09:57:16 +0100

    add pub

---
+++ pub_save/index.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+include "../classes/classes.php";
+
+$pub=new pub_manage();
+$user=new user_manager();
+$all_pub=$pub->get_all_pub_save($_SESSION['idUser']);
+
+$mode_visibiltes=$user->getModeAffichage($_SESSION['idUser']);
+
+$show=false;
+$template="pub_save";
+$page_titel="Save";
+include "../layout.phtml";
+?>
