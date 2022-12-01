e8336a8af0ce1efd50be35802345d4c45da356de
talel <talelmejri8@gmail.com>
2022-12-01 18:55:45 +0100

    logout dfix

--- logout.php
+++ logout.php
@@ -1,6 +1,7 @@
 <?php 
  session_start();
- if(!isset($_SESSION['id']) || !isset($_SESSION['idUser'])){
+ 
+ if(!isset($_SESSION['id']) && !isset($_SESSION['idUser'])){
     header("location:./auth/login");
     exit;
  }
