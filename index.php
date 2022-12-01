729a7cf34ef057ad3b6774b832d1695384cfcff9
talel <talelmejri8@gmail.com>
2022-12-01 18:55:32 +0100

    fix langue

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <?php 
  
     include "./classes/classes.php";
+    session_start();
+    
+    if(!isset($_SESSION['lang'])){
+        $_SESSION['lang']='fr';
+    }
+
+    require_once "./languages/".$_SESSION['lang'].".php";
     $user=new user_manager();
     $NbrUser=$user->countUser();
     $show=false;
