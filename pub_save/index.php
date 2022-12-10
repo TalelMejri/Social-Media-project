786b917a15695b1877181d8aba76d025655af600
talel <talelmejri8@gmail.com>
2022-12-10 02:39:29 +0100

    fix delete pub

--- pub_save/index.php
+++ pub_save/index.php
@@ -5,6 +5,28 @@ include "../classes/classes.php";
 $pub=new pub_manage();
 $user=new user_manager();
 $all_pub=$pub->get_all_pub_save($_SESSION['idUser']);
+if(!$all_pub){
+    $all_pub="vide";
+}
+
+if(isset($_GET['idpub'])){
+    $pub->deletePubSave($_GET['idpub']);
+    $all_pub=$pub->get_all_pub_save($_SESSION['idUser']);
+    if(!$all_pub){
+        $all_pub="vide";
+    }
+}
+
+function userFromId($id){
+    $user=new user_manager();
+    return $user->get_user($id);
+}
+
+function pubFromId($id){
+    $pub=new pub_manage();
+    return $pub->get_pub($id);
+}
+
 $mode_visibiltes=$user->getModeAffichage($_SESSION['idUser']);
 $show=false;    
 $template="pub_save";
