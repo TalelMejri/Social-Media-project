32d1ef000a8dd46c65d28902effd35554e59f8e3
talel <talelmejri8@gmail.com>
2022-12-12 02:30:39 +0100

    dashboard

--- ProfilAdmin/index.php
+++ ProfilAdmin/index.php
@@ -1,12 +1,24 @@
 <?php
 
     session_start();
-    if(!$_SESSION['id']){
+
+    if(!isset($_SESSION['id'])){
         header("location:../../auth/login");
         exit;
     }
+
+    include "../classes/classes.php";
+
+    $user=new user_manager();
+    $number_all_user=$user->get_number_all_user();
+    $number_all_pub=$user->get_number_all_pub();
+    $number_all_story=$user->get_number_all_story();
+    $nbr_liked=$user->get_number_all_like();
+    $nbr_comment=$user->get_number_all_comment();
+
+    
     $show=false;
-    $template="ProfilUser";
+    $template="ProfilAdmin";
     $page_titel="Bizo";
     include "../layout.phtml";
 
