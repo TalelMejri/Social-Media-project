0efa74b20917886b2fbc50e88ff412e2bfa91c2b
Talel Mejri <96019025+TalelMejri@users.noreply.github.com>
2022-12-28 21:19:38 +0100

    clear

--- delete_notif.php
+++ delete_notif.php
@@ -1,16 +1,12 @@
 <?php 
 
     include "./classes/classes.php";
-<<<<<<< HEAD
     session_start();
     
     if(!isset($_SESSION['nameUser'])){
         header("location:./auth/login");
         exit;
     }
-=======
-    
->>>>>>> 937619ed86e16409819a0302c45fdb5238fc8ef1
 
     $user=new user_manager();
     $user->delete_notif_by_id($_GET['id']);
