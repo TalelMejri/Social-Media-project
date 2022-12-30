abd67167ed2d80be83cf5a5051fdc8096a68de36
Talel Mejri <96019025+TalelMejri@users.noreply.github.com>
2022-12-30 21:31:15 +0100

    Update index.php

--- auth/login/index.php
+++ auth/login/index.php
@@ -8,23 +8,23 @@
             exit;
         }else if(empty($password)){
             header("location:index.php?msg=password required&type=danger");
-            exit;
+         
         }else{
             $user=new user_manager();
             $verifer_user=$user->loginUser($email,$password);
             $verifer_admin=$user->loginadmin($email,$password);
             if(!$verifer_admin && !$verifer_user){
                 header("location:index.php?msg=password or email is incorrect&type=danger");
-                exit;
+              
             }else if($verifer_user==2){
                 $link="<a href='../renvoyerToken'> Cliquer and renvoyer token </a>" ;
                 header("location:index.php?msg=account is not verified".$link."&type=danger");
             }else if($verifer_admin==1){
                 header("location:../../ProfilAdmin");
-                exit;
+               
             }else if($verifer_user==1){
                 header("location:../../ProfilUser");
-                exit;
+             
             }
         }
     }
